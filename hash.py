a5f41f53783b3e540b3371c7020f6a7a0364e3fe
Michael <Michael.evans1775@outlook.com>
2023-02-09 10:26:28 -0700

    wip

--- hash.py
+++ hash.py
@@ -4,4 +4,35 @@ class hashMap:
         for i in range (initCap):
             self.table.append([])
 
-            
+
+    def insert (self, key, item):
+        bucket = hash(key)%len(self.table)
+        list = self.table[bucket]
+
+        for keyPair in list:
+            if keyPair[0] == key:
+                keyPair[1] = item
+                return True
+            value = [key,item]
+            list.append(value)
+            return True
+
+    def search (self, key):
+        bucket = hash(key) % len(self.table)
+        list = self.table[bucket]
+
+        for keyPair in list:
+            if keyPair[0] == key:
+                return keyPair[1]
+            return None
+
+
+    def delete (self, key):
+        bucket = hash(key) % len(self.table)
+        list = self.table[bucket]
+
+        if key in list:
+            list.remove(key)
+
+
+    
