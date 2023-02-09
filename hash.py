4cf4131d787a70a58069736998982ef38249919b
Michael <Michael.evans1775@outlook.com>
2023-02-09 11:13:59 -0700

    hashtable imp

--- hash.py
+++ hash.py
@@ -1,38 +1,42 @@
-class hashMap:
-    def __int__(self,initCap = 10):
+class HashTable:
+
+    def __init__(self, initial_capacity=10):
         self.table = []
-        for i in range (initCap):
+        for i in range(initial_capacity):
             self.table.append([])
 
+    def insert(self, key, item):
+        #
+        bucket = hash(key) % len(self.table)
+        hashList = self.table[bucket]
 
-    def insert (self, key, item):
-        bucket = hash(key)%len(self.table)
-        list = self.table[bucket]
-
-        for keyPair in list:
+        for keyPair in hashList:
+            # print (key_value)
             if keyPair[0] == key:
                 keyPair[1] = item
                 return True
-            value = [key,item]
-            list.append(value)
-            return True
 
-    def search (self, key):
+        key_value = [key, item]
+        hashList.append(key_value)
+        return True
+
+    def search(self, key):
         bucket = hash(key) % len(self.table)
-        list = self.table[bucket]
+        hashList = self.table[bucket]
 
-        for keyPair in list:
+        for keyPair in hashList:
             if keyPair[0] == key:
                 return keyPair[1]
-            return None
+        return None
 
-
-    def delete (self, key):
+    def remove(self, key):
         bucket = hash(key) % len(self.table)
-        list = self.table[bucket]
+        hashList = self.table[bucket]
 
-        if key in list:
-            list.remove(key)
+        for keyPair in hashList:
+            if keyPair[0] == key:
+                hashList.remove([keyPair[0], keyPair[1]])
 
-
-    
+test = HashTable()
+test.insert(1,1)
+print(test.table)
